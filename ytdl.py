56039f1599773b79154d26a08819daa5d018d034
Nathan Smith <nrsmith012@gmail.com>
2017-07-01 03:29:51 -0700

    Update ytdl.py

--- ytdl.py
+++ ytdl.py
@@ -1,11 +1,14 @@
 __doc__ = """
-this script takes a url & saves YouTube or other streaming video to the Download folder on your Android device
-instructions from YouTube app: select share --> copy link to clipboard, run this script (or switch back to it)  & put in the url when prompted
+takes a url & saves YouTube or other streaming videos to Download folder on Android devices
+
+from YouTube app, select share --> copy link to clipboard, run this script & input the url when the dialog appears
+
 only for use where permitted; I take no responsibility for whether you comply with copyright law or anybody else's terms of service
+
 written for Android, on Android, summer 2017
 """ 
 
-__author__ = 'Nate Smith nrsmith012@gmail.com (ytdl.py top level script) but the real work gets done by youtube-dl https://rg3.github.io/youtube-dl/ and Android sl4a code'
+__author__ = 'Nate Smith nrsmith012@gmail.com (ytdl.py top level script) but the real work gets done by youtube-dl https://rg3.github.io/youtube-dl/ and Android sl4a code by Damon Kohler <damonkohler@gmail.com>'
 __license__ = 'Apache License, Version 2.0'
 
 import sl4a
@@ -14,9 +17,10 @@ import pip
 from subprocess import call as C
 from time import sleep as zz
 
+
 def dload(url):
   if (len(url.split(".")) < 2) and (len(url.split(":")) < 3):
-    raise ValueError("input error : %s can't be a valid
+    raise ValueError("input error : %s can't be a valid url or ip address") 
   C(["youtube-dl", 
     	"--no-check-certificate", 
     	url])
@@ -24,21 +28,19 @@ def dload(url):
     	 
 
 if __name__ == "__main__":
-  for li in __doc__.split("\n"):
-    print(li)
-    zz(3.5) # you can lower this number to make the text faster
+  print(__doc__)
   droid = sl4a.Android()
-  print("\ndroid ready+'\n'*2")
-  zz(2)
+  zz(6)
+  print("\ndroid ready"+'\n'*2)
+  zz(2.2)
   url = droid.dialogGetInput('Paste video link here').result
   zz(.77)
-  print("got URL"+'\n'*2)  
-  zz(. 77)
+  print("got yoURL"+'\n'*2)  
+  zz(.77)
   print("preparing youtube-dl") 
   zz(6)
   print("[ give it up to a minute - after that, it's best to check your network connection ] ") 
   os.chdir("/sdcard/Download")
   pip.main(["install", "youtube-dl", "--upgrade"])
-  print("pipped") 
+  print("pip has properly pipped") 
   dload(url) 
-  
